e0f04bc6e382ed98077f6d97ac6ad7b7164d0f4b
Sonam Yadav <yadusonam08@gmail.com>
2026-07-29 20:22:46 +0530

    Implement swap functionality for two numbers

--- Day02/SwapTwoNumbers.java
+++ Day02/SwapTwoNumbers.java
@@ -1,1 +1,36 @@
+/*
+Problem: Swap Two Numbers
+Concept: Variables
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class SwapTwoNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("\nBefore Swapping");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("\nAfter Swapping");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        sc.close();
+    }
+}
