2e8595afa236c0ce1db77cfe4a9ad0b76ebce2d3
Sonam Yadav <yadusonam08@gmail.com>
2026-07-29 20:21:32 +0530

    Implement Even or Odd checker using modulus operator

--- Day02/EvenOdd.java
+++ Day02/EvenOdd.java
@@ -1,1 +1,27 @@
+/*
+Problem: Even or Odd
+Concept: Modulus Operator and if-else
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+
+        sc.close();
+    }
+}
