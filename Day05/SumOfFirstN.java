fb44f7b54cc3a7f4f49b77a208c091e1e5c43411
Sonam Yadav <yadusonam08@gmail.com>
2026-08-01 07:19:46 +0530

    Add SumOfFirstN program to calculate sum of naturals
    Implemented a program to calculate the sum of the first N natural numbers using a for loop.

--- Day05/SumOfFirstN.java
+++ Day05/SumOfFirstN.java
@@ -1,1 +1,29 @@
+/*
+Problem: Sum of First N Natural Numbers
+Concept: for Loop
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class SumOfFirstN {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
