ab8abd54439026548fee2f6a0d514bad2e438e72
Sonam Yadav <yadusonam08@gmail.com>
2026-08-01 07:19:02 +0530

    Implement multiplication table generator
    Added a program to generate multiplication table for a given number.

--- Day05/MultiplicationTable.java
+++ Day05/MultiplicationTable.java
@@ -1,1 +1,27 @@
+/*
+Problem: Multiplication Table
+Concept: for Loop
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("\nMultiplication Table of " + num);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+
+        sc.close();
+    }
+}
