a3f648fd126854aa57fa933537626bd949ebcdfc
Sonam Yadav <yadusonam08@gmail.com>
2026-08-02 07:10:28 +0530

    Add ReverseNumber program to reverse an integer
    Implemented a program to reverse an integer using a while loop.

--- Day06/ReverseNumber.java
+++ Day06/ReverseNumber.java
@@ -1,1 +1,31 @@
+/*
+Problem: Reverse a Number
+Concept: while Loop
+Time Complexity: O(d)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int reverse = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number / 10;
+        }
+
+        System.out.println("Reverse = " + reverse);
+
+        sc.close();
+    }
+}
