441814aa1f2841c9acf901ad62c7408778370754
Sonam Yadav <yadusonam08@gmail.com>
2026-08-04 07:11:21 +0530

    Implement factorial calculation method
    Added a method to calculate the factorial of a number using a loop.

--- Day08/FactorialMethod.java
+++ Day08/FactorialMethod.java
@@ -1,1 +1,33 @@
+/*
+Problem: Factorial Using Method
+Concept: Methods
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class FactorialMethod {
+
+    static int factorial(int n) {
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Factorial = " + factorial(n));
+
+        sc.close();
+    }
+}
