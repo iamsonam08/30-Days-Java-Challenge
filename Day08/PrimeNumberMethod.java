c4d385ad9ac855f47af0b11bbd5d711355dba5f0
Sonam Yadav <yadusonam08@gmail.com>
2026-08-04 07:12:13 +0530

    Add Prime Number checking method
    Implement a method to check if a number is prime.

--- Day08/PrimeNumberMethod.java
+++ Day08/PrimeNumberMethod.java
@@ -1,1 +1,39 @@
+/*
+Problem: Prime Number Using Method
+Concept: Methods
+Time Complexity: O(√n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class PrimeNumberMethod {
+
+    static boolean isPrime(int n) {
+
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (isPrime(number))
+            System.out.println(number + " is a Prime Number");
+        else
+            System.out.println(number + " is Not a Prime Number");
+
+        sc.close();
+    }
+}
