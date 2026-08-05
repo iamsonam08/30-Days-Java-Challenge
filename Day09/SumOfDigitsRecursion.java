7e3701bd45da73de37ed3c8175fff87af4adc6e4
Sonam Yadav <yadusonam08@gmail.com>
2026-08-05 10:48:01 +0530

    Implement recursive sum of digits function
    This program calculates the sum of digits of a number using recursion.

--- Day09/SumOfDigitsRecursion.java
+++ Day09/SumOfDigitsRecursion.java
@@ -1,1 +1,31 @@
+/*
+Problem: Sum of Digits Using Recursion
+Concept: Recursion
+Time Complexity: O(d)
+Space Complexity: O(d)
+*/
 
+import java.util.Scanner;
+
+public class SumOfDigitsRecursion {
+
+    static int sumOfDigits(int n) {
+
+        if (n == 0)
+            return 0;
+
+        return (n % 10) + sumOfDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        System.out.println("Sum of Digits = " + sumOfDigits(number));
+
+        sc.close();
+    }
+}
