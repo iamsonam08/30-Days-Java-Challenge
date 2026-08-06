a300cb513ff3f9055846af44feb692b534833136
Sonam Yadav <yadusonam08@gmail.com>
2026-08-06 20:32:33 +0530

    Implement Largest Element finder in an array

--- Day10/LargestElement.java
+++ Day10/LargestElement.java
@@ -1,1 +1,39 @@
+/*
+Problem: Largest Element in an Array
+Concept: Arrays
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class LargestElement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest Element = " + largest);
+
+        sc.close();
+    }
+}
