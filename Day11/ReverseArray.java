63302ef8e458ea100e5b96bc14aaa7bd54637fa3
Sonam Yadav <yadusonam08@gmail.com>
2026-08-09 20:06:16 +0530

    Add ReverseArray program for array reversal
    Implemented a program to reverse an array input by the user.

--- Day11/ReverseArray.java
+++ Day11/ReverseArray.java
@@ -1,1 +1,35 @@
+/*
+Problem: Reverse an Array
+Concept: Array Traversal
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Reversed Array:");
+
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
