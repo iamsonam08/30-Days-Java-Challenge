d168e3fa06f3babc800122bc19bfb72c12d97156
Sonam Yadav <yadusonam08@gmail.com>
2026-08-09 20:07:02 +0530

    Implement sum and average calculation for an array
    This program calculates the sum and average of user-inputted integers in an array.

--- Day11/SumAndAverage.java
+++ Day11/SumAndAverage.java
@@ -1,1 +1,40 @@
+/*
+Problem: Sum and Average of an Array
+Concept: Array Traversal
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class SumAndAverage {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        double average = (double) sum / n;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+
+        sc.close();
+    }
+}
