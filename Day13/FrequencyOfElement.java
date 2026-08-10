a58346fa0018e2db0a591eaf19cd4e8fcde3030e
Sonam Yadav <yadusonam08@gmail.com>
2026-08-11 05:16:44 +0530

    Implement frequency calculation for array elements
    This program takes an array input from the user and calculates the frequency of a specified element within that array.

--- Day13/FrequencyOfElement.java
+++ Day13/FrequencyOfElement.java
@@ -1,1 +1,43 @@
+/*
+Problem: Find Frequency of an Element
+Concept: Arrays and Searching
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class FrequencyOfElement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter element to find frequency: ");
+        int target = sc.nextInt();
+
+        int frequency = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] == target) {
+                frequency++;
+            }
+        }
+
+        System.out.println("Frequency of " + target + " = " + frequency);
+
+        sc.close();
+    }
+}
