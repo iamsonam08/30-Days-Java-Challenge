9a07465b5e22789a518169ee53822320806fd324
Sonam Yadav <yadusonam08@gmail.com>
2026-08-10 06:34:19 +0530

    Add Binary Search implementation in Java
    Implement binary search algorithm with user input.

--- Day12/BinarySearch.java
+++ Day12/BinarySearch.java
@@ -1,1 +1,58 @@
+/*
+Problem: Binary Search
+Concept: Searching Algorithm
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements in sorted order:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target element: ");
+        int target = sc.nextInt();
+
+        int left = 0;
+        int right = n - 1;
+        int index = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                index = mid;
+                break;
+            } 
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
