4d862b228bfe0b837ea66f94486e12f2797f7ad5
Sonam Yadav <yadusonam08@gmail.com>
2026-08-10 06:33:42 +0530

    Update LinearSearch.java

--- Day12/LinearSearch.java
+++ Day12/LinearSearch.java
@@ -1,1 +1,48 @@
+/*
+Problem: Linear Search
+Concept: Array Traversal
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target element: ");
+        int target = sc.nextInt();
+
+        int index = -1;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] == target) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
