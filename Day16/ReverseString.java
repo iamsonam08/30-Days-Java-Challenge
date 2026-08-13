fcae991e4516686210c69d2c3102747957ad5fd7
Sonam Yadav <yadusonam08@gmail.com>
2026-08-14 04:41:17 +0530

    Add ReverseString program to reverse user input
    Implemented a Java program to reverse a string input by the user.

--- Day16/ReverseString.java
+++ Day16/ReverseString.java
@@ -1,1 +1,29 @@
+/*
+Problem: Reverse a String
+Concept: Strings
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
 
+import java.util.Scanner;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        System.out.println("Reversed String = " + reverse);
+
+        sc.close();
+    }
+}
