cae2ec0347df5bcf7c1459825b9f206e02d400d5
Sonam Yadav <yadusonam08@gmail.com>
2026-08-14 04:41:49 +0530

    Update PalindromeString.java

--- Day16/PalindromeString.java
+++ Day16/PalindromeString.java
@@ -1,1 +1,33 @@
+/*
+Problem: Check Palindrome String
+Concept: Strings
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
 
+import java.util.Scanner;
+
+public class PalindromeString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        if (str.equals(reverse)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+}
