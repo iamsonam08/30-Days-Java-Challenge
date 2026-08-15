45ffe074eba898063c78a531a80bd5e5e37593b1
Sonam Yadav <yadusonam08@gmail.com>
2026-08-15 07:08:30 +0530

    Update CharacterFrequency.java

--- Day17/CharacterFrequency.java
+++ Day17/CharacterFrequency.java
@@ -1,1 +1,35 @@
+/*
+Problem: Find Frequency of a Character
+Concept: String Traversal
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class CharacterFrequency {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.print("Enter character to find: ");
+        char target = sc.next().charAt(0);
+
+        int frequency = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) == target) {
+                frequency++;
+            }
+        }
+
+        System.out.println("Frequency of '" + target + "' = " + frequency);
+
+        sc.close();
+    }
+}
