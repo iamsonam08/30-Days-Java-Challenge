4eb1ceef3b0a44fbba42801b37736241bd1a4c27
Sonam Yadav <yadusonam08@gmail.com>
2026-08-16 07:59:07 +0530

    Update RemoveSpaces.java

--- Day18/RemoveSpaces.java
+++ Day18/RemoveSpaces.java
@@ -1,1 +1,34 @@
+/*
+Problem: Remove Spaces from a String
+Concept: String Traversal
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
 
+import java.util.Scanner;
+
+public class RemoveSpaces {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch != ' ') {
+                result = result + ch;
+            }
+        }
+
+        System.out.println("String without spaces = " + result);
+
+        sc.close();
+    }
+}
