13539c8edd0861e67ec8b285fff19b7bdf167477
Sonam Yadav <yadusonam08@gmail.com>
2026-08-16 07:59:45 +0530

    Implement word counting in CountWords.java
    Added a Java program to count the number of words in a given string by traversing the input.

--- Day18/CountWords.java
+++ Day18/CountWords.java
@@ -1,1 +1,36 @@
+/*
+Problem: Count Words in a String
+Concept: String Traversal
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class CountWords {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String str = sc.nextLine().trim();
+
+        int words = 0;
+
+        if (!str.isEmpty()) {
+            words = 1;
+
+            for (int i = 0; i < str.length(); i++) {
+
+                if (str.charAt(i) == ' ') {
+                    words++;
+                }
+            }
+        }
+
+        System.out.println("Number of words = " + words);
+
+        sc.close();
+    }
+}
