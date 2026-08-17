28ebeadcb5e0c20074943582e0606094a537e673
Sonam Yadav <yadusonam08@gmail.com>
2026-08-17 08:09:13 +0530

    Update CharacterTypeCount.java

--- Day19/CharacterTypeCount.java
+++ Day19/CharacterTypeCount.java
@@ -1,1 +1,49 @@
+/*
+Problem: Count Character Types
+Concept: Character Classification
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class CharacterTypeCount {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int uppercase = 0;
+        int lowercase = 0;
+        int digits = 0;
+        int special = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                uppercase++;
+            }
+            else if (ch >= 'a' && ch <= 'z') {
+                lowercase++;
+            }
+            else if (ch >= '0' && ch <= '9') {
+                digits++;
+            }
+            else {
+                special++;
+            }
+        }
+
+        System.out.println("Uppercase = " + uppercase);
+        System.out.println("Lowercase = " + lowercase);
+        System.out.println("Digits = " + digits);
+        System.out.println("Special Characters = " + special);
+
+        sc.close();
+    }
+}
