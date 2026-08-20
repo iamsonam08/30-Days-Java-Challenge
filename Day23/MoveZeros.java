d355076ee315b8887f543b94624b9a420a555d42
Sonam Yadav <yadusonam08@gmail.com>
2026-08-21 03:42:23 +0530

    Update MoveZeros.java

--- Day23/MoveZeros.java
+++ Day23/MoveZeros.java
@@ -1,1 +1,52 @@
+/*
+Problem: Move All Zeros to the End
+Concept: Array Manipulation
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class MoveZeros {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int index = 0;
+
+        // Place all non-zero elements first
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        // Fill remaining positions with zero
+        while (index < n) {
+            arr[index] = 0;
+            index++;
+        }
+
+        System.out.println("Array after moving zeros:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
