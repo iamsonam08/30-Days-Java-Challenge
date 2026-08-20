9e29c3a38ba2a49257f6c1e4ce4dbaafd94c9486
Sonam Yadav <yadusonam08@gmail.com>
2026-08-20 09:14:03 +0530

    Update SecondLargest.java

--- DAY22/SecondLargest.java
+++ DAY22/SecondLargest.java
@@ -1,1 +1,51 @@
+/*
+Problem: Find Second Largest Element
+Concept: Arrays
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class SecondLargest {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] > largest) {
+
+                secondLargest = largest;
+                largest = arr[i];
+
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+
+                secondLargest = arr[i];
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("Second largest element does not exist.");
+        } else {
+            System.out.println("Second Largest = " + secondLargest);
+        }
+
+        sc.close();
+    }
+}
