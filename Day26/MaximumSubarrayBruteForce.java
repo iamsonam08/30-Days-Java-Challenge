50fae8eee479ed08bcbf39a5e5a2542713e174d9
Sonam Yadav <yadusonam08@gmail.com>
2026-08-25 10:55:55 +0530

    Add brute force solution for maximum subarray sum
    Implemented a brute force approach to find the maximum subarray sum using nested loops.

--- Day26/MaximumSubarrayBruteForce.java
+++ Day26/MaximumSubarrayBruteForce.java
@@ -1,1 +1,47 @@
+/*
+Problem: Maximum Subarray Sum - Brute Force
+Concept: Nested Loops and Subarrays
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class MaximumSubarrayBruteForce {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            int currentSum = 0;
+
+            for (int j = i; j < n; j++) {
+
+                currentSum += arr[j];
+
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
+            }
+        }
+
+        System.out.println("Maximum Subarray Sum = " + maxSum);
+
+        sc.close();
+    }
+}
