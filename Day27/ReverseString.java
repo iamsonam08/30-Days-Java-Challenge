aa16403dabca9e8be14ce9bdd31c9cd25c3942cc
Sonam Yadav <yadusonam08@gmail.com>
2026-08-26 12:27:40 +0530

    Update ReverseString.java

--- Day27/ReverseString.java
+++ Day27/ReverseString.java
@@ -1,1 +1,29 @@
+/*
+Problem: Reverse a String
+Concept: Strings and Character Traversal
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
 
+import java.util.Scanner;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        System.out.println("Reversed String: " + reversed);
+
+        sc.close();
+    }
+}
