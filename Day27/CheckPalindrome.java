c5a048632d8a2c59cb8f7788dd08906551e84a20
Sonam Yadav <yadusonam08@gmail.com>
2026-08-26 12:28:18 +0530

    Update CheckPalindrome.java

--- Day27/CheckPalindrome.java
+++ Day27/CheckPalindrome.java
@@ -1,1 +1,43 @@
+/*
+Problem: Check Whether a String is Palindrome
+Concept: String Comparison and Two Pointer Technique
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class CheckPalindrome {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while (left < right) {
+
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+}
