a15e1dd69b498a0b19663bec10bf743bd8d27b51
Sonam Yadav <yadusonam08@gmail.com>
2026-08-27 10:15:21 +0530

    Update CountCharacters.java

--- Day28/CountCharacters.java
+++ Day28/CountCharacters.java
@@ -1,1 +1,54 @@
+/*
+Problem: Count Vowels, Consonants, Digits and Spaces
+Concept: String Traversal and Character Checking
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class CountCharacters {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int vowels = 0;
+        int consonants = 0;
+        int digits = 0;
+        int spaces = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = Character.toLowerCase(str.charAt(i));
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+
+                vowels++;
+
+            } else if (ch >= 'a' && ch <= 'z') {
+
+                consonants++;
+
+            } else if (ch >= '0' && ch <= '9') {
+
+                digits++;
+
+            } else if (ch == ' ') {
+
+                spaces++;
+            }
+        }
+
+        System.out.println("Vowels = " + vowels);
+        System.out.println("Consonants = " + consonants);
+        System.out.println("Digits = " + digits);
+        System.out.println("Spaces = " + spaces);
+
+        sc.close();
+    }
+}
