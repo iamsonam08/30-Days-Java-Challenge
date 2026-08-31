9ca4d33a2632a2f94d77b4dfc008088cc8f3b918
Sonam Yadav <yadusonam08@gmail.com>
2026-08-31 20:18:26 +0530

    Update TwoSum.java

--- Day29/TwoSum.java
+++ Day29/TwoSum.java
@@ -1,1 +1,58 @@
+/*
+Problem: Two Sum
+Concept: Arrays and Nested Loops
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        boolean found = false;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] + arr[j] == target) {
+
+                    System.out.println(
+                        arr[i] + " + " + arr[j] + " = " + target
+                    );
+
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No pair found.");
+        }
+
+        sc.close();
+    }
+}
