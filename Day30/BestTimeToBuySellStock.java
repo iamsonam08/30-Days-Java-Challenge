5d523ed0e75f4526692c95339dbb3b8f5829cb97
Sonam Yadav <yadusonam08@gmail.com>
2026-09-01 06:48:33 +0530

    Add solution for Best Time to Buy and Sell Stock
    Implement the solution for the Best Time to Buy and Sell Stock problem using array traversal and a greedy approach.

--- Day30/BestTimeToBuySellStock.java
+++ Day30/BestTimeToBuySellStock.java
@@ -1,1 +1,47 @@
+/*
+Problem: Best Time to Buy and Sell Stock
+Concept: Array Traversal and Greedy Approach
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+import java.util.Scanner;
+
+public class BestTimeToBuySellStock {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of days: ");
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+
+        System.out.println("Enter stock prices:");
+
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        System.out.println("Maximum Profit = " + maxProfit);
+
+        sc.close();
+    }
+}
